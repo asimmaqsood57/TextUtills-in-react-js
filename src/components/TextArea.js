bc55cea49583813f7edfc5fa6be0f023e99b7c8f
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-09-12 20:52:05 +0500

    some functionalities are done

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -4,6 +4,25 @@ function TextArea(props) {
   const upperCase = () => {
     props.settext(props.text.toUpperCase());
   };
+  const lowerCase = () => {
+    props.settext(props.text.toLowerCase());
+  };
+  const clear = () => {
+    props.settext("");
+  };
+
+  const copyText = () => {
+    let data = document.getElementById("textArea");
+
+    data.select();
+
+    navigator.clipboard.writeText(data.value);
+  };
+  //   const rmexsp = () => {
+  //     document.getElementById("textArea").value = document
+  //       .getElementById("textArea")
+  //       .innerText.trim();
+  //   };
 
   return (
     <>
@@ -17,20 +36,28 @@ function TextArea(props) {
               props.settext(e.target.value);
             }}
             class="form-control"
-            id="exampleFormControlTextarea1"
+            id="textArea"
             rows="9"
           ></textarea>
         </div>
       </div>
       <div className="container">
-        <button className="btn btn-primary" onClick={upperCase}>
+        <button className="btn btn-primary " onClick={upperCase}>
           Convert to upper case
+        </button>
+        <button className="btn btn-primary mx-3" onClick={lowerCase}>
+          Convert to Lower case
+        </button>
+        <button className="btn btn-primary " onClick={clear}>
+          Clear
+        </button>
+        <button className="btn btn-primary  mx-3" onClick={copyText}>
+          copy
         </button>
       </div>
 
       <div className="container">
         <h2 className="my-3">Preview</h2>
-
         {props.text}
       </div>
     </>
